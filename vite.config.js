57e27b35b52a599f37a3205de39cfb7756b7f50d
邱少镘 <2471745855@qq.com>
2025-11-30 01:36:58 +0800

    fix: 修复

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',  // 改为相对路径
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  server: {
-    host: true
-  }
+  base: './'
 })
